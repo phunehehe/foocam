7bd7622414a02d287159de4a51ca8bf5c507738c
Hoang Xuan Phu <phunehehe@gmail.com>
2013-08-27 00:03:45 +0700

    add some util methods

--- HDRCamera/src/main/java/net/phunehehe/hdrcamera/MainActivity.java
+++ HDRCamera/src/main/java/net/phunehehe/hdrcamera/MainActivity.java
@@ -1,10 +1,39 @@
 package net.phunehehe.hdrcamera;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /**
+     * Check if this device has a camera
+     */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +41,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
 }
