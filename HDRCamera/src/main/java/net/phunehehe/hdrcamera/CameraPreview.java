032da1ec814ca34ad560f1d1b2db379d6aea6f94
Hoang Xuan Phu <phunehehe@gmail.com>
2013-08-28 00:11:16 +0700

    clean up and refactor

--- HDRCamera/src/main/java/net/phunehehe/hdrcamera/CameraPreview.java
+++ HDRCamera/src/main/java/net/phunehehe/hdrcamera/CameraPreview.java
@@ -8,69 +8,48 @@ import android.view.SurfaceView;
 
 import java.io.IOException;
 
-/**
- * A basic Camera preview class
- */
+
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-    private SurfaceHolder mHolder;
-    private Camera mCamera;
-    private String TAG = "TAG";
+
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
-        mCamera = camera;
-
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-        // deprecated setting, but required on Android versions prior to 3.0
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        this.camera = camera;
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        // Deprecated, but required on Android versions prior to 3.0
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, now tell the camera where to draw the preview.
-        try {
-            if (mCamera == null) {
-                mCamera = Camera.open();
-            }
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
-        }
+        startPreview();
     }
 
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
-
-        if (mHolder.getSurface() == null) {
-            // preview surface does not exist
+        if (surfaceHolder.getSurface() == null) {
             return;
         }
-
-        // stop preview before making changes
+        // Stop preview before making changes
         try {
-            mCamera.stopPreview();
+            camera.stopPreview();
         } catch (Exception e) {
-            // ignore: tried to stop a non-existent preview
+            // Ignore: tried to stop a non-existent preview
         }
+        // Start preview with new settings
+        startPreview();
+    }
 
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-
-        // start preview with new settings
+    private void startPreview() {
         try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-
-        } catch (Exception e) {
-            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            Log.d(MainActivity.TAG, "Error starting camera preview: " + e.getMessage());
         }
     }
 }
--- HDRCamera/src/main/java/net/phunehehe/hdrcamera/MainActivity.java
+++ HDRCamera/src/main/java/net/phunehehe/hdrcamera/MainActivity.java
@@ -1,8 +1,6 @@
 package net.phunehehe.hdrcamera;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
@@ -20,111 +18,69 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements PictureCallback {
 
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-    private static final String TAG = "TAG";
-    private Camera mCamera;
-    private CameraPreview mPreview;
-    private PictureCallback mPicture = new PictureCallback() {
-
+    public static final String TAG = "HDR";
+    private Camera camera;
+    private View.OnClickListener captureButtonListener = new View.OnClickListener() {
         @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-            if (pictureFile == null) {
-                Log.d(TAG, "Error creating media file, check storage permissions");
-                return;
-            }
-
-            try {
-                FileOutputStream fos = new FileOutputStream(pictureFile);
-                fos.write(data);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                Log.d(TAG, "File not found: " + e.getMessage());
-            } catch (IOException e) {
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
-            }
-
-            mCamera.startPreview();
+        public void onClick(View v) {
+            camera.takePicture(null, null, MainActivity.this);
         }
     };
 
     /**
-     * A safe way to get an instance of the Camera object.
+     * Create a File for saving an image
      */
-    public static Camera getCameraInstance() {
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        } catch (Exception e) {
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
+    private static File getOutputMediaFile() {
 
-    /**
-     * Create a File for saving an image or video
-     */
-    private static File getOutputMediaFile(int type) {
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            throw new RuntimeException("External storage not available");
+        }
 
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
-                return null;
+                throw new RuntimeException("Failed to create directory: " + mediaStorageDir);
             }
         }
 
         // Create a media file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "IMG_" + timeStamp + ".jpg");
-        } else if (type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                    "VID_" + timeStamp + ".mp4");
-        } else {
-            return null;
-        }
+        return new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+    }
 
-        return mediaFile;
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        try {
+            File pictureFile = getOutputMediaFile();
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            fos.write(data);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        } catch (RuntimeException e) {
+            Log.d(TAG, "Error creating media file: " + e.getMessage());
+        }
+        MainActivity.this.camera.startPreview();
     }
 
     private void releaseCamera() {
-        if (mCamera != null) {
-            mCamera.release();        // release the camera for other applications
-            mCamera = null;
+        if (camera != null) {
+            camera.release();
+            camera = null;
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        releaseCamera();              // release the camera immediately on pause event
-    }
-
-    /**
-     * Check if this device has a camera
-     */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
+        releaseCamera();
     }
 
     @Override
@@ -132,30 +88,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
+        camera = Camera.open();
 
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
+        CameraPreview cameraPreview = new CameraPreview(this, camera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        preview.addView(cameraPreview);
 
-        // Add a listener to the Capture button
         Button captureButton = (Button) findViewById(R.id.button_capture);
-        captureButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        mCamera.takePicture(null, null, mPicture);
-                    }
-                }
-        );
+        captureButton.setOnClickListener(captureButtonListener);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
