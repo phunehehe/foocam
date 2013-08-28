58bc8679a11b9301fd1889da67ee5ff9ce515b58
Hoang Xuan Phu <phunehehe@gmail.com>
2013-08-28 22:22:23 +0700

    multiple pictures

--- HDRCamera/src/main/java/net/phunehehe/hdrcamera/MainActivity.java
+++ HDRCamera/src/main/java/net/phunehehe/hdrcamera/MainActivity.java
@@ -1,8 +1,10 @@
 package net.phunehehe.hdrcamera;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -16,16 +18,27 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MainActivity extends Activity implements PictureCallback {
 
     public static final String TAG = "HDR";
     private Camera camera;
+    private Camera.Parameters parameters;
+    private Queue<Integer> exposureValues;
+    private FrameLayout preview;
     private View.OnClickListener captureButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            camera.takePicture(null, null, MainActivity.this);
+            parameters = camera.getParameters();
+            exposureValues = new LinkedList<Integer>(Arrays.asList(
+                    parameters.getMinExposureCompensation(),
+                    0,
+                    parameters.getMaxExposureCompensation()));
+            processQueue();
         }
     };
 
@@ -38,8 +51,7 @@ public class MainActivity extends Activity implements PictureCallback {
             throw new RuntimeException("External storage not available");
         }
 
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        File mediaStorageDir = getMediaStorageDir();
 
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists()) {
@@ -51,6 +63,22 @@ public class MainActivity extends Activity implements PictureCallback {
         // Create a media file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         return new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+    }
+
+    private static File getMediaStorageDir() {
+        return new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+    }
+
+    private boolean processQueue() {
+        Integer exposureValue = exposureValues.poll();
+        if (exposureValue == null) {
+            return false;
+        }
+        parameters.setExposureCompensation(exposureValue);
+        camera.setParameters(parameters);
+        camera.takePicture(null, null, MainActivity.this);
+        return true;
     }
 
     @Override
@@ -68,6 +96,10 @@ public class MainActivity extends Activity implements PictureCallback {
             Log.d(TAG, "Error creating media file: " + e.getMessage());
         }
         MainActivity.this.camera.startPreview();
+        if (!processQueue()) {
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED,
+                    Uri.fromFile(getMediaStorageDir())));
+        }
     }
 
     private void releaseCamera() {
@@ -75,6 +107,7 @@ public class MainActivity extends Activity implements PictureCallback {
             camera.release();
             camera = null;
         }
+        preview.removeAllViews();
     }
 
     @Override
@@ -84,15 +117,19 @@ public class MainActivity extends Activity implements PictureCallback {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        camera = Camera.open();
+        CameraPreview cameraPreview = new CameraPreview(this, camera);
+        preview.addView(cameraPreview);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        camera = Camera.open();
-
-        CameraPreview cameraPreview = new CameraPreview(this, camera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(cameraPreview);
+        preview = (FrameLayout) findViewById(R.id.camera_preview);
 
         Button captureButton = (Button) findViewById(R.id.button_capture);
         captureButton.setOnClickListener(captureButtonListener);
